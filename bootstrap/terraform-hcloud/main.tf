38632fa36cdf504cf7cf18dc70f46165493fec96
Titas Janušois <titas.januson@gmail.com>
2026-04-21 17:12:39 +0200

    Add tailscale setup

--- bootstrap/terraform-hcloud/main.tf
+++ bootstrap/terraform-hcloud/main.tf
@@ -36,12 +36,6 @@ resource "hcloud_firewall" "vps" {
     source_ips = ["0.0.0.0/0", "::/0"]
   }
 
-  rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "6443"
-    source_ips = var.kube_api_allowed_cidrs
-  }
 }
 
 resource "hcloud_server" "vps" {
